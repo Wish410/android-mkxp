c25823957b82a79548d2dab42f6741b70e143bab
Varun Patil <radialapps@gmail.com>
2019-05-02 18:46:28 +0530

    Fixup java stuff

---
+++ compile.ps1
@@ -0,0 +1,4 @@
+cd jni
+ndk-build -j3
+cd ..
+cp -R -Force libs ../aveyond1-android/
--- jni/Application.mk
+++ jni/Application.mk
@@ -2,4 +2,4 @@ APP_ABI := armeabi-v7a
 APP_PLATFORM := android-19
 APP_OPTIM := release
 APP_STL := c++_static
-APP_CPPFLAGS := -std=c++1z -frtti -fexceptions
+APP_CPPFLAGS := -std=c++14 -frtti -fexceptions
