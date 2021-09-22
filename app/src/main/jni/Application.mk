3bf7be1c2245015dd766f9f3c67aa19fe82039d5
JoiPlay <joiplay@protonmmail.com>
2021-09-22 14:18:03 +0300

    Remove unused/unmodified third party libraries from repository

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,17 @@
-APP_ABI := armeabi-v7a arm64-v8a
-APP_PLATFORM := android-19
+APP_ABI := armeabi-v7a arm64-v8a x86_64
+APP_PLATFORM := android-21
 APP_OPTIM := release
 APP_STL := c++_static
-APP_CPPFLAGS := -std=c++14 -frtti -fexceptions
+APP_CPPFLAGS := -std=c++11 -frtti -fexceptions -Os -fPIC
+
+ifeq ($(TARGET_ARCH_ABI), armeabi)
+	APP_CPPFLAGS += -DARCH_32BIT -DABI_ARMEABI_V7A
+else ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	APP_CPPFLAGS += -DARCH_32BIT -DABI_ARMEABI_V7A
+else ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
+	APP_CPPFLAGS += -DARCH_64BIT -DABI_ARM64_V8A
+else ifeq ($(TARGET_ARCH_ABI), x86)
+	APP_CPPFLAGS += -DARCH_32BIT -DABI_X86
+else ifeq ($(TARGET_ARCH_ABI), x86_64)
+	APP_CPPFLAGS += -DARCH_64BIT -DABI_X86_64
+endif
