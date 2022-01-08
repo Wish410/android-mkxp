ab6a932c7136ca8ddce11cd5bcf6e9d6dabce6bb
joiplay <joiplay@protonmail.com>
2022-01-08 12:59:48 +0300

    Don't build for x86_64 and update flags

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,17 +1,12 @@
-APP_ABI := armeabi-v7a arm64-v8a x86_64
+APP_ABI := armeabi-v7a arm64-v8a #x86_64
 APP_PLATFORM := android-21
 APP_OPTIM := release
-APP_CPPFLAGS := -std=c++11 -frtti -fexceptions -Os -fPIC
 APP_STL := c++_shared
+APP_CPPFLAGS := -std=c++11 -frtti -fexceptions -O1 -ffunction-sections -fdata-sections
+APP_CFLAGS := -O1 -ffunction-sections -fdata-sections
 
-ifeq ($(TARGET_ARCH_ABI), armeabi)
-	APP_CPPFLAGS += -DARCH_32BIT -DABI_ARMEABI_V7A
-else ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-	APP_CPPFLAGS += -DARCH_32BIT -DABI_ARMEABI_V7A
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	APP_CPPFLAGS += -DARCH_32BIT -DABI_ARMEABI_V7A -march=armv7-a -mfpu=neon -mfloat-abi=softfp
 else ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-	APP_CPPFLAGS += -DARCH_64BIT -DABI_ARM64_V8A
-else ifeq ($(TARGET_ARCH_ABI), x86)
-	APP_CPPFLAGS += -DARCH_32BIT -DABI_X86
-else ifeq ($(TARGET_ARCH_ABI), x86_64)
-	APP_CPPFLAGS += -DARCH_64BIT -DABI_X86_64
+	APP_CPPFLAGS += -DARCH_64BIT -DABI_ARM64_V8A -march=armv8-a
 endif
